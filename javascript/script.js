afc1fc4c54c6df96a1d42475babd21c1156e79c7
Aline Douaki <123590731+alinedka@users.noreply.github.com>
2024-06-12 18:46:25 -0300

    improvements

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var currentPath = window.location.pathname;
-    currentPath = currentPath.substring(currentPath.lastIndexOf('/') + 1);
-
-
-    var navLinks = document.querySelectorAll('.content .list-menu li a');
-
-    navLinks.forEach(function(link) {
-        var linkPath = link.pathname;
-        linkPath = linkPath.substring(linkPath.lastIndexOf('/') + 1);
-
-        if (linkPath === currentPath) {
-            link.classList.add('active');
+// Script para adicionar a classe 'active' ao link da página atual
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll(".content .list-menu li a");
+    const currentPath = window.location.pathname;
+    
+    links.forEach(link => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
         }
     });
 });
